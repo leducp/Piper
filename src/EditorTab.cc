d669df0a1c778994d46debc487554df476edf231
Philippe Leduc <philippe.leduc@mailfence.com>
2020-03-25 00:31:01 +0100

    Handle tab renaming.

--- src/EditorTab.cc
+++ src/EditorTab.cc
@@ -1,11 +1,27 @@
 #include "EditorTab.h"
+#include "EditorWidget.h"
 
 #include <QDebug>
 #include <QTabBar> 
 #include <QToolButton>
+#include <QEvent>
+#include <QFocusEvent>
 
 namespace piper
 {
+    class TabHeaderEdit : public QLineEdit
+    {
+    public:
+        TabHeaderEdit(QWidget* parent = nullptr) : QLineEdit(parent) { }
+        virtual ~TabHeaderEdit() = default;
+        
+        void mousePressEvent(QMouseEvent *event) override
+        {
+            QLineEdit::mousePressEvent(event);
+            QWidget::mousePressEvent(event); // let the parent do its work too !
+        }
+    };
+    
     EditorTab::EditorTab(QWidget* parent)
         : QTabWidget(parent)
     {
@@ -13,11 +29,35 @@ namespace piper
         QPushButton* tb = new QPushButton();
         tb->setFlat(true);
         tb->setIcon(QIcon(":/icon/add_tab.svg"));
-        tb->setStyleSheet("* { icon-size: 24px 24px; }");
+        tb->setStyleSheet("* { icon-size: 30px 30px; }");
         setCornerWidget(tb, Qt::TopLeftCorner);
+
+        // Ensure that button will be always visible.
+        setStyleSheet("QTabBar::tab { height: 40px; }");
+        tb->setMinimumSize(40, 40);
+        tabBar()->setMinimumSize(40, 40);
         
-        tb->setMinimumSize(28, 28);
-        tabBar()->setMinimumSize(28, 28);
+        // Connect signal to manage tab creation/rename/deletion
+        QObject::connect(tb,   &QPushButton::clicked,            this, &EditorTab::createNewEditorTab);
+        QObject::connect(this, &QTabWidget::tabCloseRequested,   this, &EditorTab::closeEditorTab);
+    }
+    
+    
+    void EditorTab::createNewEditorTab()
+    {
+        EditorWidget* editor = new EditorWidget();
+        int32_t index = addTab(editor, "");
+        
+        QLineEdit* edit = new TabHeaderEdit();
+        edit->setText("pipeline " + QString::number(index));
+        edit->setFrame(false);
+        edit->setStyleSheet("QLineEdit { background:transparent; }");
+        tabBar()->setTabButton(index, QTabBar::LeftSide, edit);
     }
 
+    
+    void EditorTab::closeEditorTab(int32_t index)
+    {
+        delete widget(index); // Note: removeTab do not destroy the widget.
+    }
 }
--- src/EditorTab.h
+++ src/EditorTab.h
@@ -3,18 +3,22 @@
 
 #include <QTabWidget>
 #include <QPushButton>
+#include <QLineEdit>
 
 namespace piper
 {
     class EditorTab : public QTabWidget
     {
+        Q_OBJECT
     public:
         EditorTab(QWidget* parent = nullptr);
         virtual ~EditorTab() = default;
         
+    private slots:
+        void createNewEditorTab();
+        void closeEditorTab(int32_t index);
+        
     private:
-        
-        
     };
     
 }
